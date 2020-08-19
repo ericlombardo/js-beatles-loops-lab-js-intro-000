be11cb6b765ef3fa5369c3477b20878f9cf9f9ed
sincere-trainer-5337 <eric.m.lombardo@gmail.com>
2020-08-19 18:50:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // add solution here
+function theBeatlesPlay(musicianArray, instrumentArray) {
+  // empty list to put the full strings in
+  var musiciansPlay = [];
+  // for loop through musicianArray
+  for (var i = 0; i < musicianArray.length; i++) {
+    musiciansPlay.push(`${musicianArray[i]} plays ${instrumentArray[i]}`);
+  }
+
+  // return the final string of what musicians play
+  return musiciansPlay;
+}
